c0ba7745596b0718617dd54637e3b165b18ee8cd
ingdanielb <danielb2889@hotmail.com>
2023-12-20 18:28:17 -0500

    Delete unnecessary files and code

--- instructors/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [      
-    path('', views.index_instructors, name='instructors'),
-    path('schedule/', views.schedule, name='schedule'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('schedule_down/', views.schedule_down, name='schedule_down_instructor'),       
-]
